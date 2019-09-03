0416fecbc807cb31f5bd5f89cdf84160e8c40fed
Michael Hashizume <michael.hashizume@puppet.com>
2019-09-03 11:38:48 -0700

    Puppet lint run

--- manifests/backup_profile.pp
+++ manifests/backup_profile.pp
@@ -13,18 +13,18 @@ define encrypted_backup::backup_profile (
   $commands_pre   = [],
   $commands_post  = [],
 ) {
-  file {"/opt/encrypted_backup/profile_${title}.sh":
-    ensure   => present,
-    mode     => '700',
-    content  => epp('encrypted_backup/encrypted_backup.epp', {
-                server_url     => $server_url,
-                key_auth       => $key_auth,
-                backup_dir     => $backup_dir,
-                temp_dir       => $temp_dir,
-                tar_name       => $tar_name,
-                gpg_recipient  => $gpg_recipient,
-                commands_pre   => $commands_pre,
-                commands_post  => $commands_post,
+  file { "/opt/encrypted_backup/profile_${title}.sh":
+    ensure  => present,
+    mode    => '0700',
+    content => epp('encrypted_backup/encrypted_backup.epp', {
+                server_url    => $server_url,
+                key_auth      => $key_auth,
+                backup_dir    => $backup_dir,
+                temp_dir      => $temp_dir,
+                tar_name      => $tar_name,
+                gpg_recipient => $gpg_recipient,
+                commands_pre  => $commands_pre,
+                commands_post => $commands_post,
                 }),
   }
 
@@ -37,13 +37,13 @@ define encrypted_backup::backup_profile (
   }
 
   cron { "encrypted_backup_${title}":
-    command   => "bash -x /opt/encrypted_backup/profile_${title}.sh >> /opt/encrypted_backup/profile_${title}.log 2>&1",
-    user      => 'root',
-    hour      => $cron_hour,
-    minute    => $cron_minute,
-    monthday  => $cron_monthday,
-    month     => $cron_month,
-    weekday   => $cron_weekday,
+    command  => "bash -x /opt/encrypted_backup/profile_${title}.sh >> /opt/encrypted_backup/profile_${title}.log 2>&1",
+    user     => 'root',
+    hour     => $cron_hour,
+    minute   => $cron_minute,
+    monthday => $cron_monthday,
+    month    => $cron_month,
+    weekday  => $cron_weekday,
   }
 
 }
